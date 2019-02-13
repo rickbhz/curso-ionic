d09cdb5271474a776f2dc71089a45fee63f88b1c
Carlos Salgado <carlos.d.salgado@gmail.com>
2019-02-13 08:57:14 -0200

    Categorias DTO

--- src/main/java/com/cadu/cursomc/services/CategoriaService.java
+++ src/main/java/com/cadu/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.cadu.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e ){
 			throw new DataIntegrityException("Nao e possivel excluir uma categoria com produtos associados.");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
 	}
 }
